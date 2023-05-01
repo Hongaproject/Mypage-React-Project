49cbf98a3f55e5eb21e2664b0ad46d725c7c0dae
Hongaproject <tkwlscjq99@gmail.com>
2023-05-01 13:43:00 +0900

    11day movie

--- src/components/Content/Movie/Movie.jsx
+++ src/components/Content/Movie/Movie.jsx
@@ -1,13 +1,37 @@
-import { HashRouter} from "react-router-dom";
-import MovieSearch from "./MovieSearch";
-
+import { useState } from "react";
+import { useEffect } from "react";
 
 function Movie () {
-    return(
-        <HashRouter>
-            <MovieSearch />
-        </HashRouter>
+
+    const KEY = process.env.REACT_APP_MOVIE_KEY;
+    const DATE = "20230430";
+
+    const [loading,setLoading] = useState(true);
+    const [movies,setMovies] = useState([]);
+
+    const getMovies = async() => {
+        const json = await (await fetch(`http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${KEY}&targetDt=${DATE}`)).json();
+        
+        setMovies(json.boxOfficeResult.dailyBoxOfficeList);
+        setLoading(false);
+    }
+    useEffect(() => {
+        getMovies();
+    }, []);
+
+    return (
+        <div>
+            {loading ? <h1>Loading...</h1> :
+            <div>{movies.map(movie => 
+                <div key={movie.movieCd}> 
+                    <h3>{movie.movieNm}</h3> 
+                    <p>누적 관객 수 : {movie.audiAcc}명</p>
+                    <p>영화 개봉일 :  ({movie.openDt}) </p>
+                </div>)} 
+            </div>}
+        </div>
     );
 }
+
 
 export default Movie;
